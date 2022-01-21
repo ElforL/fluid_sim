479c047612145887a4174f65549f68e8e54d42b2
Laith Shono <elfordev@gmail.com>
2022-01-21 23:52:02 +0300

    📝 Implement simulator

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:math' as math;
 
+import 'package:fluid_sim/simulator.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -32,65 +33,113 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  bool isPlaying = false;
-
-  late List<List<double>> array;
+  late Simulator sim;
 
   @override
   void initState() {
-    array = List.generate(20, (index) => List.generate(20, (index) => math.Random().nextDouble()));
+    sim = Simulator(10, 10);
+    populateSim();
     super.initState();
+  }
+
+  void populateSim() {
+    sim.setCell(4, 4, 1);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('🌊'),
-      ),
       body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(10),
-          child: ClipRRect(
-            child: SizedBox(
-              width: 700,
-              height: 700,
-              child: CustomPaint(
-                painter: MyPainter(
-                  array,
-                  tileWidth: 35,
-                  tileHeight: 35,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Liquid Simulation'.toUpperCase(),
+              style: Theme.of(context).textTheme.headline4?.copyWith(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 2,
+                  ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: ClipRRect(
+                child: SizedBox(
+                  width: 700,
+                  height: 700,
+                  child: CustomPaint(
+                    painter: MyPainter(
+                      sim: sim,
+                      tileWidth: 70,
+                      tileHeight: 70,
+                    ),
+                  ),
                 ),
               ),
             ),
-          ),
+            AnimatedBuilder(
+              animation: sim,
+              builder: (context, child) {
+                return Text(
+                  'Current iteration: ${sim.iteration}',
+                  style: Theme.of(context).textTheme.subtitle1,
+                );
+              },
+            ),
+          ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            isPlaying = !isPlaying;
-          });
-        },
-        tooltip: 'Start/Pause',
-        child: Icon(isPlaying ? Icons.pause_rounded : Icons.play_arrow_rounded),
+      floatingActionButton: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          FloatingActionButton(
+            mini: true,
+            onPressed: () {
+              sim.stop();
+              populateSim();
+            },
+            tooltip: 'Replay',
+            child: const Icon(Icons.replay_rounded),
+          ),
+          FloatingActionButton(
+            mini: true,
+            onPressed: () {
+              sim.tick();
+            },
+            tooltip: 'Skip',
+            child: const Icon(Icons.skip_next_rounded),
+          ),
+          FloatingActionButton(
+            mini: true,
+            onPressed: () {
+              if (sim.isRunning) {
+                sim.pause();
+              } else {
+                sim.start();
+              }
+              setState(() {});
+            },
+            tooltip: 'Start/Pause',
+            child: Icon(sim.isRunning ? Icons.pause_rounded : Icons.play_arrow_rounded),
+          ),
+        ],
       ),
     );
   }
 }
 
 class MyPainter extends CustomPainter {
-  MyPainter(
-    this.array, {
-    Listenable? repaint,
+  MyPainter({
+    required this.sim,
     this.fillColor = Colors.white60,
     this.emptyColor = Colors.white12,
     required this.tileWidth,
     required this.tileHeight,
     this.backgroundColor = Colors.black38,
-  }) : super(repaint: repaint);
+  }) : super(repaint: sim);
 
-  final List<List<double>> array;
+  final Simulator sim;
+  List<List<double>> get array => sim.array;
 
   final Color? backgroundColor;
 
@@ -113,35 +162,26 @@ class MyPainter extends CustomPainter {
 
     for (var i = 0; i < array.length; i++) {
       for (var j = 0; j < array[i].length; j++) {
-        final Rect r1 = Offset(i * tileWidth, j * tileHeight) & Size(tileWidth, tileHeight);
+        final Rect r1 = Offset(i * tileHeight, j * tileWidth) & Size(tileWidth, tileHeight);
 
         // the gradient shows the filled amount by having both stops at the [percent]
-        final double percent = array[i][j];
+        final double percent = array[j][i];
         final gradient = LinearGradient(
           transform: const GradientRotation(-math.pi / 2),
-          stops: [
-            percent,
-            percent,
-          ],
-          colors: [
-            fillColor,
-            emptyColor,
-          ],
+          stops: [percent, percent],
+          colors: [fillColor, emptyColor],
         );
         // apply shader
         var paint2 = Paint()..shader = gradient.createShader(r1);
 
         // paint
-        canvas.drawRect(
-          r1,
-          paint2,
-        );
+        canvas.drawRect(r1, paint2);
       }
     }
   }
 
   @override
-  bool shouldRepaint(covariant CustomPainter oldDelegate) {
-    return false;
+  bool shouldRepaint(covariant MyPainter oldDelegate) {
+    return oldDelegate.array != array;
   }
 }
