529a00785ba51a0e713e8c6781940cace44d9a83
Laith Shono <elfordev@gmail.com>
2022-01-24 02:46:57 +0300

    📝 Remove the array reference in the Cell class

--- lib/models/cell.dart
+++ lib/models/cell.dart
@@ -10,9 +10,6 @@ class Cell {
   /// The directions the liquid is flowing.
   FlowDirections flowDirections = FlowDirections();
 
-  /// The array the cell is in.
-  final List<List<Cell>> _array;
-
   /// The x coordinate of the cell in the array.
   int x;
 
@@ -20,24 +17,11 @@ class Cell {
   int y;
 
   Cell(
-    this._array,
     this.x,
     this.y, {
     this.level = 0,
     this.type = CellType.nonSolid,
   });
-
-  /// The bottom neighbour cell.
-  Cell get bottom => _array[y + 1][x];
-
-  /// The top neighbour cell.
-  Cell get top => _array[y - 1][x];
-
-  /// The right neighbour cell.
-  Cell get right => _array[y][x - 1];
-
-  /// The left neighbour cell.
-  Cell get left => _array[y][x + 1];
 
   @override
   String toString() {
