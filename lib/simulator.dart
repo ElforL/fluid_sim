c16aecec306c92db3de5ee54c5aee0f73ac0efef
Laith Shono <elfordev@gmail.com>
2022-01-24 03:10:30 +0300

    📝 Add doc comments to Simulator methods

--- lib/simulator.dart
+++ lib/simulator.dart
@@ -8,15 +8,18 @@ class Simulator extends ChangeNotifier {
     this.height, {
     this.tickDuration = const Duration(milliseconds: 50),
   })  : array = [],
-        diffs = [] {
+        _diffs = [] {
     _resetDiffs();
-    _fillArrayWithCells(array, height, width);
+    _fillArrayWithCells(height, width);
   }
 
+  /// The minimum amount of liquid a cell can contain
   static const minLvl = 0.001;
+
+  /// The maximum amount of liquid a cell can contain without compression.
   static const maxLvl = 1;
 
-  /// The duration that the simulator will wait before starting the next tick
+  /// The duration that the simulator will wait before starting the next tick.
   Duration tickDuration;
 
   /// the number of the current iteration.
@@ -24,37 +27,49 @@ class Simulator extends ChangeNotifier {
 
   int width, height;
 
-  final List<List<Cell>> array;
-  final List<List<double>> diffs;
-
   /// is the simulation currently running (i.e., the main loop is active);
   bool isRunning = false;
 
+  /// The [width] x [height] 2D array that contains all the cells.
+  final List<List<Cell>> array;
+
+  /// The difference of liquid levels to be applied to [array] cells at the end of each tick.
+  final List<List<double>> _diffs;
+
+  /// Changes the width and height of the [array] **and resets them all**.
+  ///
+  /// All cells will turn to empty non-solid cells.
+  ///
+  /// Throws an [Exception] when called and [isRunning] is `true`.
   void changeWidthHeight({int? newWidth, int? newHeight}) {
     if (isRunning) throw Exception("Can't change size while running");
     width = newWidth ?? width;
     height = newHeight ?? height;
-    _fillArrayWithCells(array, height, width);
+    _fillArrayWithCells(height, width);
   }
 
+  /// Start the simulator/main loop
   void start() {
     isRunning = true;
     _loop();
   }
 
+  /// Pause the simulator/main loop
   void pause() {
     isRunning = false;
     notifyListeners();
   }
 
+  /// Stop the simulator, reset the [iteration] counter, and reset all the cells.
   void stop() {
-    iteration = 0;
     isRunning = false;
-    _fillArrayWithCells(array, height, width);
+    iteration = 0;
+    _fillArrayWithCells(height, width);
     notifyListeners();
   }
 
-  setCellLevel(int x, int y, double lvl) {
+  /// Set the level of the cell at [x],[y]
+  void setCellLevel(int x, int y, double lvl) {
     array[y][x].level = lvl;
     notifyListeners();
   }
@@ -159,54 +174,54 @@ class Simulator extends ChangeNotifier {
       for (var x = 0; x < array[y].length; x++) {
         var cell = array[y][x];
 
-        cell.level += diffs[y][x];
+        cell.level += _diffs[y][x];
         if (cell.level < minLvl) cell.level = 0;
       }
     }
 
     notifyListeners();
-    await Future.delayed(tickDuration);
   }
 
   /// The main loop
   void _loop() async {
     while (isRunning) {
       await tick();
+      await Future.delayed(tickDuration);
     }
   }
 
   _addDiff(int x, int y, double diff) {
-    diffs[y][x] += diff;
+    _diffs[y][x] += diff;
   }
 
-  /// returns the value of the cell below x,y
+  /// returns the cell below the cell at x,y
   ///
-  /// returns -1 if it doesn't exist
+  /// returns `null` if it doesn't exist
   Cell? _cellBelow(int x, int y) {
     _inBoundCheck(x, y);
     if (y == height - 1) return null;
     return array[y + 1][x];
   }
 
-  /// returns the value of the cell to the left of x,y
+  /// returns the cell to the left of the cell at x,y
   ///
-  /// returns -1 if it doesn't exist
+  /// returns `null` if it doesn't exist
   Cell? _cellLeft(int x, int y) {
     _inBoundCheck(x, y);
     if (x == 0) return null;
     return array[y][x - 1];
   }
 
-  /// returns the value of the cell to the right of x,y
+  /// returns the cell to the right of the cell at x,y
   ///
-  /// returns -1 if it doesn't exist
+  /// returns `null` if it doesn't exist
   Cell? _cellRight(int x, int y) {
     _inBoundCheck(x, y);
     if (x == width - 1) return null;
     return array[y][x + 1];
   }
 
-  /// Throws an [ArgumentError] if the cell [x],[y] is not in bounds.
+  /// Throws an [ArgumentError] if the coordinates [x],[y] are not in bounds.
   void _inBoundCheck(int x, int y) {
     if (x >= width || x < 0 || y >= height || y < 0) {
       throw ArgumentError('Index out of bounds. ($x,$y) in a ${width}x$height array.');
@@ -214,7 +229,7 @@ class Simulator extends ChangeNotifier {
   }
 
   /// Modifies [array] to be a [height]x[width] 2D list filled with `Cell(x,y)`
-  static void _fillArrayWithCells(final List<List<Cell>> array, int height, int width) {
+  void _fillArrayWithCells(int height, int width) {
     // Clear te array.
     // This make the array = []
     array.removeRange(0, array.length);
@@ -231,19 +246,20 @@ class Simulator extends ChangeNotifier {
     }
   }
 
+  /// Resets all elements in [_diffs] to zeros.
   void _resetDiffs() {
     // Clear te array.
     // This will make diffs = []
-    diffs.removeRange(0, diffs.length);
+    _diffs.removeRange(0, _diffs.length);
 
     // populate it
     for (var y = 0; y < array.length; y++) {
       // add a new row
-      diffs.insert(y, []);
+      _diffs.insert(y, []);
 
       // fill the row
       for (var x = 0; x < array[y].length; x++) {
-        diffs[y].insert(x, 0.0);
+        _diffs[y].insert(x, 0.0);
       }
     }
   }
